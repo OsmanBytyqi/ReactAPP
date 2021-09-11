de72982bc83bef727f138e8c20813b466e12d0ca
OsmanBytyqi <obytyqi2001@gmail.com>
2021-09-11 19:48:55 +0200

    dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+
+  const title="My component";
+  //we use dynamic values in template just for string and number, for object and boolean values we cant
+
+  const number=10;
   return (
     <div className="App">
       <div className="content">
-        <h1>My component</h1>
+        <h1>{title}</h1>
+        <p>{number}</p>
+        <p>{'hello there '}</p>
       </div>
 
      
