7e813e2750524e182c1a5105d03f50685194d550
OsmanBytyqi <obytyqi2001@gmail.com>
2021-09-13 22:48:03 +0200

    useEffect

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (  
 
         <nav className="navbar">
-            <h1>my Blog</h1>
+            <h1>My Blog</h1>
             <div className="links">
             <a href="/">Home</a>
             <a href="/create">New Blog</a>
--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogList from "./Bloglist";
 
 
@@ -13,10 +13,13 @@ const Home = () => {
 
     const handledelete=(id)=>{
       const newBlogs=blogs.filter(blog=>blog.id!==id);
-      setBlogs(newBlogs)
+      setBlogs(newBlogs);
 
-    }
-   
+    };
+
+   useEffect(()=>{
+     console.log("use efect ran");
+   })
   
     return (
       <div className="home">
