f8d068be19049c3e9944a2dcfc52532731fe0516
OsmanBytyqi <obytyqi2001@gmail.com>
2021-09-18 23:10:05 +0200

    fixing a couple of typos

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,7 @@
 const NotFound = () => {
     return ( 
 
-        <div className="not-founf">
+        <div className="not-found">
 
             <h2>Sorry</h2>
             <p>your page not found :)</p>
